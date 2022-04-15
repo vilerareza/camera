12973fcd41f883b5b82e4dd5c3602f09f1d20a5c
Reza Vilera <vilera_reza99@yahoo.com>
2022-04-15 22:14:56 +0700

    stopping camera when qr detected

--- qrscan.py
+++ qrscan.py
@@ -1,9 +1,8 @@
 import io
-#from functools import partial
-#from threading import Thread
 import numpy as np
 import cv2 as cv
 from camera import Camera
+import json
 
 camera = Camera()
 frame_size = (320, 240)
@@ -11,42 +10,40 @@ frame_rate = 10
 
 class StreamingOutput(object):
     # Streaming output object
-    
-    def __init__(self):
+    def __init__(self, camera):
         self.buffer = io.BytesIO()
         self.qrDetector = cv.QRCodeDetector()
         self.bufferReady = True
+        self.camera = camera
 
     def write(self, buf):
-        #if self.bufferReady:
         if buf.startswith(b'\xff\xd8'):
             # New frame
             self.buffer.truncate()
             # Reading QR
             frame = self.buffer.getvalue()
-            #t_read_qr = Thread(target = partial(self.read_qr, frame))
             self.read_qr(frame)
-            #t_read_qr.start()
             print ('ok')
             # Reset buffer pointer
             self.buffer.seek(0)
         return self.buffer.write(buf)
 
     def read_qr(self, frame):
-        #self.bufferReady = False
         npFrame = np.asarray(bytearray(frame))
         if npFrame.any():
             try:
                 img = cv.imdecode(npFrame, 1)
                 data, _, __ = self.qrDetector.detectAndDecode(img)
                 if data:
-                    print (data)                     
+                    data = json.loads(data)
+                    print (data)
+                    # Stopping camera
+                    self.camera.stop_camera()                     
             except Exception as e:
                 print (f'Qr detection error {e}')
-        #self.bufferReady = True
 
 # Initialize streaming output object
-output = StreamingOutput()
+output = StreamingOutput(camera)
 
 camera.start_camera(output, frame_size = frame_size, frame_rate = frame_rate)
 camera.wait_recording(60)
