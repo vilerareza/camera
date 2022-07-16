129587d3be3709c43e3d094b65274102e616fae2
Reza Vilera <vilera_reza99@yahoo.com>
2022-07-16 12:22:01 +0700

    create vscamera_ws3

--- vscamera_ws3.py
+++ vscamera_ws3.py
@@ -23,19 +23,19 @@ try:
     camera.start_camera(output, frame_size = frame_size, frame_rate = frame_rate)
     
     # Websocket App
-    wsapp = websocket.WebSocketApp(f"ws://{serverHost}/ws/device1/", on_message=on_message)
+    wsapp = websocket.WebSocketApp(f"ws://{serverHost}/ws/control/device1/", on_message=on_message)
     try:
         # Run the websocket in different thread
         wst = Thread(target = wsapp.run_forever)
         wst.start()
     except Exception as e:
-        print (f'{e}: Failed starting websocket connection. closing connection')
+        print (f'{e}: Failed starting websocketapp connection. closing connection')
         wsapp.close()
         wst = None
     
     # Websocket
     ws = websocket.WebSocket()
-    ws.connect(f"ws://{serverHost}/ws/device1/")
+    ws.connect(f"ws://{serverHost}/ws/frame/device1/")
 
     while True:
         with output.condition:
