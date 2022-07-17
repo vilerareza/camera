52f73acca45576f47ab8afd8a786c3208ac84c31
Reza Vilera <vilera_reza99@yahoo.com>
2022-07-17 21:01:23 +0700

    create servo_new

--- servo_new.py
+++ servo_new.py
@@ -6,7 +6,14 @@ from adafruit_servokit import ServoKit
 class Servo():
     # Servo parameter
     moveThread = None
-    posParam = {'posXMin':0, 'posXMax':180, 'posYMin':0, 'posYMax': 180, 'stepDegree':0.5, 'delayS':0.005}
+    #posParam = {'posXMin':0, 'posXMax':180, 'posYMin':0, 'posYMax': 180, 'stepDegree':0.5, 'delayS':0.005}
+    posXMin = float(0)
+    posXMax = float(180) 
+    posYMin = float(0)
+    posYMax =  float(180) 
+    stepDegree = float(0.5)
+    delayS = float(0.005)
+
 
     def __init__(self) -> None:
         self.kit = ServoKit(channels = 16)
@@ -22,22 +29,22 @@ class Servo():
         def move_x(distance):
             if distance > 0:
                 # Positive direction
-                if self.kit.servo[0].angle < self.posParam['posXMax']:
+                if self.kit.servo[0].angle < self.posXMax:
                     # Still within x movement range
                     # Calculate target position
-                    if (self.kit.servo[0].angle + distance) >= self.posParam['posXMax']:
+                    if (self.kit.servo[0].angle + distance) >= self.posXMax:
                         # Limit the movement to max limit.
-                        targetPos = self.posParam['posXMax']
+                        targetPos = self.posXMax
                     else:
                         targetPos = self.kit.servo[0].angle + distance
                     # Move X
                     while self.kit.servo[0].angle <= targetPos:
-                        currentPos = self.kit.servo[0].angle + self.posParam['stepDegree']
-                        self.kit.servo[0].angle += self.posParam['stepDegree']
+                        currentPos = self.kit.servo[0].angle + self.stepDegree
+                        self.kit.servo[0].angle += self.stepDegree
                         print (f'targetPos: {targetPos}')
                         print (f'currentPos: {currentPos}')
                         print (f'posX: {self.kit.servo[0].angle}')
-                        time.sleep(self.posParam['delayS'])
+                        time.sleep(self.delayS)
 
                     self.moveThread = None
                 else:
@@ -46,19 +53,19 @@ class Servo():
 
             elif distance < 0:
                 # Negative direction
-                if self.kit.servo[0].angle > self.posParam['posXMin']:
+                if self.kit.servo[0].angle > self.posXMin:
                     # Still within x movement range
                     # Calculate target position
-                    if (self.kit.servo[0].angle + distance) >= self.posParam['posXMin']:
+                    if (self.kit.servo[0].angle + distance) >= self.posXMin:
                         # Limit the movement to max limit.
-                        targetPos = self.posParam['posXMin']
+                        targetPos = self.posXMin
                     else:
                         targetPos = self.kit.servo[0].angle + distance
                     # Move X
                     while self.kit.servo[0].angle <= targetPos:
-                        self.kit.servo[0].angle += self.posParam['stepDegree']
+                        self.kit.servo[0].angle += self.stepDegree
                         print (f'posX: {self.kit.servo[0].angle}')
-                        time.sleep(self.posParam['delayS'])
+                        time.sleep(self.delayS)
 
                     self.moveThread = None
                 else:
