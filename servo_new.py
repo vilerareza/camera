43aa05dae0c8ba44be8371a8aaf597179e068420
Reza Vilera <vilera_reza99@yahoo.com>
2022-07-17 20:14:46 +0700

    create servo_new

---
+++ servo_new.py
@@ -0,0 +1,47 @@
+import time
+from functools import partial
+from threading import Thread
+from adafruit_servokit import ServoKit
+
+class Servo():
+    # Servo parameter
+    moveThread = None
+    posParam = {'posXMin':0, 'posXMax':180, 'posYMin':0, 'posYMax': 180, 'stepDegree':0.5, 'delayS':0.005}
+
+    def __init__(self) -> None:
+        self.kit = ServoKit(channels = 16)
+        self.center()
+        time.sleep(0.3)
+        print ('servo init ok')
+
+    def center(self, center_position = 90):
+       self.kit.servo[0].angle = center_position
+       self.kit.servo[1].angle = center_position
+
+    def start_move_x(self, distance):
+        def move_x(distance):
+            if distance > 0:
+                # Positive direction
+                if self.kit.servo[0].angle < self.posMax:
+                    # Still within x movement range
+                    # Calculate target position
+                    if (self.currentPos + distance) >= self.posMax:
+                        # Limit the movement to max limit.
+                        targetPos = self.posParam['posXMax']
+                    else:
+                        targetPos = self.currentPos + distance
+                    # Move X
+                    while self.kit.servo[0].angle <= targetPos:
+                        self.kit.servo[0].angle += self.posParam['stepDegree']
+                        print (f'posX: {self.kit.servo[0].angle}')
+                        time.sleep(self.posParam['delayS'])
+
+                    self.moveThread = None
+                else:
+                    # Dont move, already at max position
+                    self.moveThread = None
+        # Start the move thread
+        if not self.moveThread:
+            self.moveThread = Thread(target = partial(move_x, distance))
+            self.moveThread.start()
+
