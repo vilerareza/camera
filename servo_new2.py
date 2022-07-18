ba9233199426d1baa01e2827bd4b85cf6d55228b
Reza Vilera <vilera_reza99@yahoo.com>
2022-07-18 11:19:50 +0700

    create servo_new2

--- servo_new2.py
+++ servo_new2.py
@@ -1,6 +1,6 @@
 import time
 from functools import partial
-from threading import Thread
+from threading import Thread, Condition
 from adafruit_servokit import ServoKit
 
 class Servo():
@@ -14,6 +14,7 @@ class Servo():
     def __init__(self, channel = 0) -> None:
         self.kit = ServoKit(channels = 16)
         self.channel = channel
+        self.condition = Condition()
         self.center()
         time.sleep(0.3)
         print ('servo init ok')
@@ -22,68 +23,86 @@ class Servo():
        self.kit.servo[self.channel].angle = center_position
 
     def start_move(self, distance = 0, pos=None):
-        def move(distance, pos):
-
-            if pos:
-                distance = pos - self.kit.servo[self.channel].angle
-        
-            if distance > 0:
-                # Positive direction
-                if self.kit.servo[self.channel].angle < self.posMax:
-                    # Still within x movement range
-                    # Calculate target position
-                    if (self.kit.servo[self.channel].angle + distance) >= self.posMax:
-                        # Limit the movement to max limit.
-                        targetPos = self.posMax
-                    else:
-                        targetPos = self.kit.servo[self.channel].angle + distance
-                    # Move
+        #def move(distance, pos):
+        if pos:
+            distance = pos - self.kit.servo[self.channel].angle
+        if distance > 0:
+            # Positive direction
+            if self.kit.servo[self.channel].angle < self.posMax:
+                # Still within x movement range
+                # Calculate target position
+                if (self.kit.servo[self.channel].angle + distance) >= self.posMax:
+                    # Limit the movement to max limit.
+                    targetPos = self.posMax
+                else:
+                    targetPos = self.kit.servo[self.channel].angle + distance
+                # Move
+                with self.condition:
                     while self.kit.servo[self.channel].angle <= targetPos:
                         self.kit.servo[self.channel].angle += self.stepDegree
                         print (f'targetPos: {targetPos}')
                         print (f'pos: {self.kit.servo[self.channel].angle}')
                         time.sleep(self.delayS)
-                        self.moveThread = None
-                else:
-                    # Dont move, already at max position
-                    self.moveThread = None
+                    self.condition.notify_all()
+            else:
+                # Dont move, already at max position
+                pass
 
-            elif distance < 0:
-                # Negative direction
-                if self.kit.servo[self.channel].angle > self.posMin:
-                    # Still within x movement range
-                    # Calculate target position
-                    if (self.kit.servo[self.channel].angle + distance) <= self.posMin:
-                        # Limit the movement to min limit.
-                        targetPos = self.posMin
-                    else:
-                        targetPos = self.kit.servo[self.channel].angle + distance
-                    # Move
+        elif distance < 0:
+            # Negative direction
+            if self.kit.servo[self.channel].angle > self.posMin:
+                # Still within x movement range
+                # Calculate target position
+                if (self.kit.servo[self.channel].angle + distance) <= self.posMin:
+                    # Limit the movement to min limit.
+                    targetPos = self.posMin
+                else:
+                    targetPos = self.kit.servo[self.channel].angle + distance
+                # Move
+                with self.condition:
                     while self.kit.servo[self.channel].angle >= targetPos:
                         self.kit.servo[self.channel].angle -= self.stepDegree
                         print (f'targetPos: {targetPos}')
                         print (f'pos: {self.kit.servo[self.channel].angle}')
                         time.sleep(self.delayS)
-                    self.moveThread = None
-                else:
-                    # Dont move, already at max position
-                    self.moveThread = None
-        # Start the move thread
-        # if not self.moveThread:
-        #     self.moveThread = Thread(target = partial(move_x, distance))
-        #     self.moveThread.start()
-        Thread(target = partial(move, distance, pos)).start()
+                    self.condition.notify_all()
+            else:
+                # Dont move, already at min position
+                pass
 
- 
+        # with self.condition:
+        #     Thread(target = partial(move, distance, pos)).start()
+
 def excercise():
     servoX = Servo(channel=0)
     servoY = Servo(channel=1)
 
-    #while True:
-    print ('movex')
-    servoX.start_move(pos = 170)
-    servoX.start_move(pos = 10)
-    print ('movey')
-    servoY.start_move(pos = 75)
-    servoY.start_move(pos = 115)
+    def runX():
+        while True:
+            try:
+                print ('movex')
+                servoX.start_move(pos = 170)
+                with servoX.condition:
+                    servoX.condition.wait()
+                servoX.start_move(pos = 10)
+                with servoX.condition:
+                    servoX.condition.wait()
+            except:
+                break
+
+    def runY():
+        while True:
+            try:
+                print ('movey')
+                servoY.start_move(pos = 115)
+                with servoY.condition:
+                    servoY.condition.wait()
+                servoY.start_move(pos = 75)
+                with servoY.condition:
+                    servoY.condition.wait()
+            except:
+                break
+
+    Thread(target = runX).start()
+    Thread(target = runY).start()
 
