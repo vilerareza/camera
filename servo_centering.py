de0187656517d4dc7044a482cccc1e8ebf89ad30
Reza Vilera <vilera_reza99@yahoo.com>
2023-02-28 09:40:20 +0700

    servo centering

--- servo_centering.py
+++ servo_centering.py
@@ -23,6 +23,50 @@ class Servo():
         print(self.kit.servo[self.channel].angle)
         self.start_move(pos = center_position)
 
+    def start_move(self, distance = 0, pos=None):
+        #def move(distance, pos):
+        if pos:
+            distance = pos - self.kit.servo[self.channel].angle
+        if distance > 0:
+            # Positive direction
+            if self.kit.servo[self.channel].angle < self.posMax:
+                # Still within x movement range
+                # Calculate target position
+                if (self.kit.servo[self.channel].angle + distance) >= self.posMax:
+                    # Limit the movement to max limit.
+                    targetPos = self.posMax
+                else:
+                    targetPos = self.kit.servo[self.channel].angle + distance
+                # Move
+                while self.kit.servo[self.channel].angle <= targetPos:
+                    self.kit.servo[self.channel].angle += self.stepDegree
+                    #print (f'targetPos: {targetPos}')
+                    #print (f'pos: {self.kit.servo[self.channel].angle}')
+                    time.sleep(self.delayS)
+            else:
+                # Dont move, already at max position
+                pass
+
+        elif distance < 0:
+            # Negative direction
+            if self.kit.servo[self.channel].angle > self.posMin:
+                # Still within x movement range
+                # Calculate target position
+                if (self.kit.servo[self.channel].angle + distance) <= self.posMin:
+                    # Limit the movement to min limit.
+                    targetPos = self.posMin
+                else:
+                    targetPos = self.kit.servo[self.channel].angle + distance
+                # Move
+                while self.kit.servo[self.channel].angle >= targetPos:
+                    self.kit.servo[self.channel].angle -= self.stepDegree
+                    #print (f'targetPos: {targetPos}')
+                    #print (f'pos: {self.kit.servo[self.channel].angle}')
+                    time.sleep(self.delayS)
+            else:
+                # Dont move, already at min position
+                pass
+
 
 servoX = Servo(channel=0)
 servoY = Servo(channel=1)
